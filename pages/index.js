5658deb123b03e0665c228d5dfb5eae9ce48291f
mizuki <trombonelover8888@gmail.com>
2023-05-02 13:17:09 +0900

    [add] init eslint

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,16 @@ import styles from "./index.module.css";
 import Header from "../Component/Header";
 import BlogList from "../Component/BlogList";
 import Footer from "../Component/Footer";
-import HeadContent from "../Component/HeadContent"
+import HeadContent from "../Component/HeadContent";
 
 export const postDatabaseId = process.env.NOTION_BLOG_DATABASE_ID;
 
 export default function Home({ posts }) {
   return (
     <div>
-      <HeadContent title='Top' />
+      <HeadContent title="Top" />
       <Header />
-      <main className={`${styles.container}`} >
+      <main className={`${styles.container}`}>
         <BlogList posts={posts} admin={false} />
       </main>
       <Footer />
@@ -23,14 +23,13 @@ export default function Home({ posts }) {
 //ISRを追加:バックグラウンドでrevalidateに設定した秒数を超すとHTMLの再レンダリングを行なってくれるもの
 // 動的ページ（このようなブログサイト）に有用
 export const getStaticProps = async () => {
-  const postsDatabase = await getDatabase(postDatabaseId)
+  const postsDatabase = await getDatabase(postDatabaseId);
   return {
     props: {
       posts: postsDatabase,
     },
     // アクセスしたこの値の間は同じキャッシュを返す
     // この秒数を超すと新しいキャッシュを構築し直す
-    revalidate: 10
-  }
+    revalidate: 10,
+  };
 };
-
