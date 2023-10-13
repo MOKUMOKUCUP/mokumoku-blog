333fc211a00594fd77ebb8fa1f7ab4574722301b
Mizuki <mbaba@kanolab.net>
2023-10-13 22:01:14 +0900

    [fix] change image domain

--- next.config.js
+++ next.config.js
@@ -3,13 +3,12 @@
  */
 
 // next.config.js
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
 
 module.exports = withBundleAnalyzer({
-    images: {
-        domains: ['s3.us-west-2.amazonaws.com'],
-    },
+  images: {
+    domains: ["prod-files-secure.s3.us-west-2.amazonaws.com"],
+  },
 });
-
